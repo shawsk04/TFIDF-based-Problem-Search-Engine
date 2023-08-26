9f1c2cb5425024a25e7d6cc2d9a31e7bd53d3cb4
shawsk04 <shawsk04@gmail.com>
2023-08-26 22:35:20 +0530

    minor changes

--- helpers/responseGenerator.js
+++ helpers/responseGenerator.js
@@ -7,7 +7,6 @@ const {findFrequency} = require('./findFrequency');
 const N = 3285;
 
 problemDatabasePath = path.join(__dirname, '..', 'database/problems/')
-// console.log(problemDatabasePath)
 
 const responseArrayGenerator = (question, uniquekeywords, IDF, TFIDF_array, magnitude, problem_titles, problem_urls) =>{
     // console.log(question)
@@ -39,16 +38,20 @@ const responseArrayGenerator = (question, uniquekeywords, IDF, TFIDF_array, magn
 
     for(let i = 0; i < queryKW_unique.length; i++){
         let kWindex = uniquekeywords.indexOf(queryKW_unique[i]);
+
         console.log("uniquekeyword ",uniquekeywords)
         console.log("index of ",queryKW_unique[i],"->",kWindex)
+
         if(kWindex != -1){
             let t = (frequency[i] * parseFloat(IDF[kWindex])) / queryKW_unique.length;
             magnitude_TFIDF_vector += t * t;
             query_TFIDF_vector.push([kWindex, t]);
         }
     }
+
     console.log("query_TFIDF_vector", query_TFIDF_vector)
     console.log("magnitude_TFIDF_vector", magnitude_TFIDF_vector)
+
     magnitude_TFIDF_vector = Math.sqrt(magnitude_TFIDF_vector);
 
     for(let i = 1; i<N + 1; i++){
@@ -65,14 +68,13 @@ const responseArrayGenerator = (question, uniquekeywords, IDF, TFIDF_array, magn
             let similarity = dotPdt / (magnitude_TFIDF_vector * parseFloat(magnitude[i - 1]));
             matchedFileIndexWithSimilarity.push([similarity, i]);
         }
-        
     }
 
     var sortedArray = matchedFileIndexWithSimilarity.sort(function(a, b) {
         return b[0] - a[0];
     });
 
-    console.log("sorted array", sortedArray)
+    // console.log("sorted array", sortedArray)
 
     let responseArray = [];
     
@@ -89,7 +91,7 @@ const responseArrayGenerator = (question, uniquekeywords, IDF, TFIDF_array, magn
     }
     const arrayUniqueByKey = [...new Map(responseArray.map(item => [item["title"], item])).values()];
 
-    console.log("arrayUniqueByKey", arrayUniqueByKey)
+    // console.log("arrayUniqueByKey", arrayUniqueByKey)
 
     return arrayUniqueByKey;
 }
