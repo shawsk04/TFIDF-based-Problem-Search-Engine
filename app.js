46a8382e58c4790bbf1c229cec58d3ef4f5e13aa
shawsk04 <shawsk04@gmail.com>
2023-08-27 11:12:59 +0530

    added search result page

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const path = require("path"); 
+const path = require("path")
+const {getProblemList} = require("./helpers/getProblemList")
+
 
 // setting up app
 const app = express();
@@ -19,4 +21,14 @@ app.listen(port, () => {
 //GET request to home page
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.post("/search", (req, res) => {
+  const query = req.body.question;
+  const question = query.toLowerCase();
+  const arr = getProblemList(question);
+  console.log(arr);
+
+  res.render("searchResult",{resp: arr});
+
 });
